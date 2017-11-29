66f7b5fc95bf8029fcd68e6209dfcb67890c51c7
Ashley Rolleston <ash@block-tree.io>
2017-11-29 17:35:17 +0000

    clear previous private net
    run and claim gas

--- test.sh
+++ test.sh
@@ -22,13 +22,21 @@ docker run --name neo-publish neo-publish
 echo "Copying published zip file from container..."
 docker cp neo-publish:/opt/neo-cli.zip /opt/neo-cli.zip
 
+echo "Removing existing privatenet files and images..."
+rm -rf ./neo-privatenet-docker
+docker stop neo-privnet
+docker rm neo-privnet
+docker rmi neo-privnet
+
 echo "Cloning latest neo-privatenet-docker repo..."
 #git clone https://github.com/CityOfZion/neo-privatenet-docker.git
 git clone https://github.com/AshRolls/neo-privatenet-docker.git
 git checkout specify_neocli
 
-echo "Building docker privatenet with new neo-cli"
+echo "Building docker privatenet with new neo-cli..."
 ./neo-privatenet-docker/docker_build.sh /opt/neo-cli-built.zip
 
+echo "Running Privatenet and claiming GAS..."
+./neo-privatenet-docker/docker_run_and_create_wallet.sh
 
 
