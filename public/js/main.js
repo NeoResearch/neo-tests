b3df660da64d42b6aa769e88c1393ef32bc8a415
Vitor <vncoelho@gmail.com>
2018-04-10 17:07:35 -0300

    Frontend template of NeoCompiler copied to this project - The idea is to design a frontend basic interface for extracting some results and graphs

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+/* Main  */
+var app = angular.module('neoOptTestsIoWebApp', [
+  'ngRoute'
+]);
+
+var NEOSCAN_PATH = "https://neoscan.neocompiler.io";
+var NODES_CSHARP_PATH = "http://neocompiler.io:30333";
+
+var LOCAL_DEVELOPMENT = false;
+if(this.window.location.href.indexOf("localhost")  != -1)
+	LOCAL_DEVELOPMENT = true;
+
+if(LOCAL_DEVELOPMENT){
+	NEOSCAN_PATH = "http://localhost:4000";
+	NODES_CSHARP_PATH = "http://localhost:30333";
+	//NEOSCAN_PATH = "https://neoscan.neocompiler.io";
+	NODES_CSHARP_PATH = "https://node1.neocompiler.io";
+}
+
+/* Routes */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+
+    //home page
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+
+    // Pages
+    .when("/neoeditor", {templateUrl: "partials/neoeditor.html", controller: "PageCtrl"})
+    .when("/utils", {templateUrl: "partials/utils.html", controller: "PageCtrl"})
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/* Controls all other Pages */
+app.controller('PageCtrl', function ( $scope, /*$location, */$http) {
+  // Activates Tooltips for Social Links
+  //$('.tooltip-social').tooltip({
+  //  selector: "a[data-toggle=tooltip]"
+  //})
+});
