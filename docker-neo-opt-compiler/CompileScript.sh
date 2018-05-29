c01051b5e6d7a6bc4599540eea3ab7afe1ea8d32
Igor Machado <igor.machado@gmail.com>
2018-05-28 21:10:04 -0300

    informing dotnet commands

--- docker-neo-opt-compiler/CompileScript.sh
+++ docker-neo-opt-compiler/CompileScript.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # remove neo-cli package and reference to local neo
+echo "dotnet remove /opt/neo-cli/neo-cli/neo-cli.csproj package neo"
 dotnet remove /opt/neo-cli/neo-cli/neo-cli.csproj package neo
+
+echo "dotnet sln /opt/neo-cli/neo-cli.sln add /opt/neo/neo/neo.csproj"
 dotnet sln /opt/neo-cli/neo-cli.sln add /opt/neo/neo/neo.csproj
+
+echo "dotnet add /opt/neo-cli/neo-cli/neo-cli.csproj reference /opt/neo/neo/neo.csproj"
 dotnet add /opt/neo-cli/neo-cli/neo-cli.csproj reference /opt/neo/neo/neo.csproj
 
 #cd /opt/neo
@@ -13,5 +18,8 @@ dotnet add /opt/neo-cli/neo-cli/neo-cli.csproj reference /opt/neo/neo/neo.csproj
 #cp neo/bin/Release/netstandard2.0/* build/
 
 # publish
+echo "dotnet publish /opt/neo-cli/neo-cli/neo-cli.csproj -o neo-cli -c Release -r ubuntu.16.04-x64"
 dotnet publish /opt/neo-cli/neo-cli/neo-cli.csproj -o neo-cli -c Release -r ubuntu.16.04-x64
+
+echo "cd /opt/neo-cli/neo-cli && zip -r /opt/build/neo-cli-built.zip neo-cli"
 cd /opt/neo-cli/neo-cli && zip -r /opt/build/neo-cli-built.zip neo-cli
