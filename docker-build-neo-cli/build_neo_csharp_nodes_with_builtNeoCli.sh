30a2310bdcc5f6cd17baba2289dafcdc2332a4c2
Vitor <vncoelho@gmail.com>
2018-06-13 08:33:43 -0300

    Improvements and neo.vm package adjustments are now possible

--- docker-build-neo-cli/build_neo_csharp_nodes_with_builtNeoCli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATH_CSHARP_NODES="../../A-neocompiler-eco/docker-neo-csharp-nodes/"
-
-cp ./neo-cli-built.zip $PATH_CSHARP_NODES
-
-echo "Building docker neo-csharp-nodes with specific neo-cli."
-(cd $PATH_CSHARP_NODES/; ./docker_build.sh --neo-cli ./neo-cli-built.zip)
