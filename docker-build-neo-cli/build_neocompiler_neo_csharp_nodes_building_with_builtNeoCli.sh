f93ccdbc43e9cfa805884f37c4238739c6b141b2
Vitor <vncoelho@gmail.com>
2018-07-02 14:03:01 -0300

    More generalizations for Neo 3.0

--- docker-build-neo-cli/build_neocompiler_neo_csharp_nodes_building_with_builtNeoCli.sh
+++ docker-build-neo-cli/build_neocompiler_neo_csharp_nodes_building_with_builtNeoCli.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PATH_CSHARP_NODES="../../A-neocompiler-eco/docker-neo-csharp-nodes/"
 
+rm $PATH_CSHARP_NODES/neo-cli.zip
+rm $PATH_CSHARP_NODES/neo-cli-built.zip
 cp ./neo-cli-built.zip $PATH_CSHARP_NODES
 
 echo "Building docker neo-csharp-nodes with specific neo-cli."
