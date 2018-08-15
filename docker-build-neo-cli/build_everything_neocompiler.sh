25b85340c6da4de16bca5654d3df228f86c2d4d0
Vitor <vncoelho@gmail.com>
2018-08-15 09:02:45 -0300

    Cleanning

--- docker-build-neo-cli/build_everything_neocompiler.sh
+++ docker-build-neo-cli/build_everything_neocompiler.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 PATH_NEOCOMPILER="../../A-neocompiler-eco"
-PATH_CSHARP_NODES=$PATH_NEOCOMPILER"/docker-neo-csharp-node"
-echo "Nodes path is: " + $PATH_CSHARP_NODES
+PATH_CSHARP_NODE=$PATH_NEOCOMPILER"/docker-neo-csharp-node"
+echo "Nodes path is: " + $PATH_CSHARP_NODE
 
-rm $PATH_CSHARP_NODES/neo-cli.zip
-rm $PATH_CSHARP_NODES/neo-cli-built.zip
-cp ./neo-cli-built.zip $PATH_CSHARP_NODES/neo-cli-built.zip
+rm $PATH_CSHARP_NODE/neo-cli.zip
+rm $PATH_CSHARP_NODE/neo-cli-built.zip
+cp ./neo-cli-built.zip $PATH_CSHARP_NODE/neo-cli-built.zip
 
 echo "Building docker neo-csharp-nodes with specific neo-cli."
 (cd $PATH_NEOCOMPILER; ./build_everything.sh --dev)
--- docker-build-neo-cli/build_neocompiler_neo_csharp_nodes_building_with_builtNeoCli.sh
+++ docker-build-neo-cli/build_neocompiler_neo_csharp_nodes_building_with_builtNeoCli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PATH_CSHARP_NODES="../../A-neocompiler-eco/docker-neo-csharp-node"
+PATH_CSHARP_NODE="../../A-neocompiler-eco/docker-neo-csharp-node"
 
-rm $PATH_CSHARP_NODES/neo-cli.zip
-rm $PATH_CSHARP_NODES/neo-cli-built.zip
-cp ./neo-cli-built.zip $PATH_CSHARP_NODES/neo-cli-built.zip
+rm $PATH_CSHARP_NODE/neo-cli.zip
+rm $PATH_CSHARP_NODE/neo-cli-built.zip
+cp ./neo-cli-built.zip $PATH_CSHARP_NODE/neo-cli-built.zip
 
-echo "Building docker neo-csharp-nodes with specific neo-cli."
-(cd $PATH_CSHARP_NODES; ./docker_build.sh --neo-cli ./neo-cli-built.zip)
+echo "Building docker neo-csharp-node with specific neo-cli."
+(cd $PATH_CSHARP_NODE; ./docker_build.sh --neo-cli ./neo-cli-built.zip)
