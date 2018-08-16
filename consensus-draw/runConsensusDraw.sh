061050d2773e3af4077d5a7dc01c99c152130298
Igor Machado <igor.machado@gmail.com>
2018-08-16 20:24:30 -0300

    automatically generating example

--- consensus-draw/runConsensusDraw.sh
+++ consensus-draw/runConsensusDraw.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# generate example if does not exist
+if [ ! -f ./getremotelog/log1.logjs ]; then
+    echo "generating example!"
+    ./generateExample.sh
+fi
+
 DOOR_CONSENSUS_DRAW=20000
 
 echo "fuser -k on port $DOOR_CONSENSUS_DRAW"
