00a2f99e27e3a67226f859ee45c8e01e0b8861db
Vitor <vncoelho@gmail.com>
2018-08-22 19:57:12 -0300

    Improving echo

--- forking-tests/time2fork-simplified.sh
+++ forking-tests/time2fork-simplified.sh
@@ -35,11 +35,6 @@ echo "will block node3<->node4 (node4)"
 docker exec -it eco-neo-csharp-node4-running iptables -I INPUT -p tcp --dport 20335:20335 -j REJECT
 docker exec -it eco-neo-csharp-node4-running iptables -I OUTPUT -p tcp --dport 20335:20335 -j REJECT
 
-echo "forcing delay of node4 to be longer than node3 - node1To6s"
-#docker exec -it eco-neo-csharp-node3-running tc qdisc add dev eth0 root netem delay 2000ms
-#docker exec -it eco-neo-csharp-node4-running tc qdisc add dev eth0 root netem delay 4000ms
-#docker exec -it eco-neo-csharp-node1-running tc qdisc add dev eth0 root netem delay 6000ms
-
 echo ""
 echo "will wait for node1 to receive prepare response from node3 index=3"
 while [ `cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node1/*.log | tail -4 | grep height=$HEIGHT |grep index=3 | grep OnPrepareResponse | wc -l` -eq 0 ]; do
@@ -51,24 +46,28 @@ docker exec -it eco-neo-csharp-node1-running ifconfig eth0 down
 cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node1/*.log | tail -4 | grep index=3 | grep OnPrepareResponse
 docker exec -it eco-neo-csharp-node3-running ifconfig eth0 down
 
+echo ""
 echo "will wait for node2 to timeout"
 while [ `cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node2/*.log | tail -3 | grep timeout | wc -l` -eq 0 ]; do
    :  # no operation
 done
 cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node2/*.log | tail -3 | grep timeout
 
+echo ""
 echo "will wait for node3 to timeout"
 while [ `cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node3/*.log | tail -3 | grep timeout | wc -l` -eq 0 ]; do
    :  # no operation
 done
 cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node3/*.log | tail -3 | grep timeout
 
+echo ""
 echo "will wait for node4 to timeout"
 while [ `cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node4/*.log | tail -3 | grep timeout | wc -l` -eq 0 ]; do
    :  # no operation
 done
 cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node4/*.log | tail -3 | grep timeout
 
+echo ""
 echo "Sleeping 30s..."
 sleep 30
 
@@ -77,19 +76,13 @@ docker exec -it eco-neo-csharp-node2-running ifconfig eth0 up
 docker exec -it eco-neo-csharp-node3-running ifconfig eth0 up
 docker exec -it eco-neo-csharp-node4-running ifconfig eth0 up
 
-sudo echo "I AM UNBLOCKED (node3 and node4 are back)!" >> $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node2/2018-08-18.log
-
-#echo "will wait for node2 index=0 to become primary nv=1"
-#while [ `cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node2/*.log | tail -1 | grep initialize | grep index=0 | grep view=1 | grep Primary | wc -l` -eq 0 ]; do
-#   :  # no operation
-#done
-
 echo ""
 echo "will wait for node2 to relay block"
 while [ `cat $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node2/*.log | tail -3 | grep relay | wc -l` -eq 0 ]; do
    :  # no operation
 done
 
+echo ""
 echo "unblock node3<->node4"
 docker exec -it eco-neo-csharp-node3-running iptables -D INPUT -p tcp --dport 20336:20336 -j ACCEPT
 docker exec -it eco-neo-csharp-node3-running iptables -D OUTPUT -p tcp --dport 20336:20336 -j ACCEPT
@@ -98,7 +91,5 @@ docker exec -it eco-neo-csharp-node4-running iptables -D OUTPUT -p tcp --dport 2
 
 echo "unblock node1"
 docker exec -it eco-neo-csharp-node1-running ifconfig eth0 up
-
-sudo echo "I AM UNBLOCKED!" >> $PATH_NEOCOMPILER_ECO/docker-compose-eco-network/logs-neocli-node1/2018-08-17.log
 
 echo "Byebye. Node1 should be stuck"
