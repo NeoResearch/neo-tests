0d72f3ca7e9db3629d9cdc39f7f22f2af5ae21da
Vitor <vncoelho@gmail.com>
2018-08-22 19:14:58 -0300

    Forks were again reproduced!

--- forking-tests/unlockall.sh
+++ forking-tests/unlockall.sh
@@ -7,28 +7,28 @@ docker exec -it eco-neo-csharp-node3-running ifconfig eth0 up
 docker exec -it eco-neo-csharp-node4-running ifconfig eth0 up
 
 echo "unblock node1"
-docker exec -it eco-neo-csharp-node1-running iptables -D INPUT -p tcp --dport 20333:20336 -j DROP
-docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20333:20336 -j DROP
+docker exec -it eco-neo-csharp-node1-running iptables -D INPUT -p tcp --dport 20333:20336 -j ACCEPT
+docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20333:20336 -j ACCEPT
 
 echo "unblock node2"
-docker exec -it eco-neo-csharp-node2-running iptables -D INPUT -p tcp --dport 20333:20336 -j DROP
-docker exec -it eco-neo-csharp-node2-running iptables -D OUTPUT -p tcp --dport 20333:20336 -j DROP
+docker exec -it eco-neo-csharp-node2-running iptables -D INPUT -p tcp --dport 20333:20336 -j ACCEPT
+docker exec -it eco-neo-csharp-node2-running iptables -D OUTPUT -p tcp --dport 20333:20336 -j ACCEPT
 
 echo "unforbid communications with node2"
-docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20334:20334 -j DROP
-docker exec -it eco-neo-csharp-node3-running iptables -D OUTPUT -p tcp --dport 20334:20334 -j DROP
-docker exec -it eco-neo-csharp-node4-running iptables -D OUTPUT -p tcp --dport 20334:20334 -j DROP
+docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20334:20334 -j ACCEPT
+docker exec -it eco-neo-csharp-node3-running iptables -D OUTPUT -p tcp --dport 20334:20334 -j ACCEPT
+docker exec -it eco-neo-csharp-node4-running iptables -D OUTPUT -p tcp --dport 20334:20334 -j ACCEPT
 
 echo "unblock node3 <-> node4"
-docker exec -it eco-neo-csharp-node3-running iptables -D INPUT -p tcp --dport 20336:20336 -j DROP
-docker exec -it eco-neo-csharp-node3-running iptables -D OUTPUT -p tcp --dport 20336:20336 -j DROP
-docker exec -it eco-neo-csharp-node4-running iptables -D INPUT -p tcp --dport 20335:20335 -j DROP
-docker exec -it eco-neo-csharp-node4-running iptables -D OUTPUT -p tcp --dport 20335:20335 -j DROP
+docker exec -it eco-neo-csharp-node3-running iptables -D INPUT -p tcp --dport 20336:20336 -j ACCEPT
+docker exec -it eco-neo-csharp-node3-running iptables -D OUTPUT -p tcp --dport 20336:20336 -j ACCEPT
+docker exec -it eco-neo-csharp-node4-running iptables -D INPUT -p tcp --dport 20335:20335 -j ACCEPT
+docker exec -it eco-neo-csharp-node4-running iptables -D OUTPUT -p tcp --dport 20335:20335 -j ACCEPT
 
 echo "unblock node1 -> (node3,node4)"
-docker exec -it eco-neo-csharp-node3-running iptables -D INPUT -p tcp --dport 20333:20333 -j DROP
-docker exec -it eco-neo-csharp-node4-running iptables -D INPUT -p tcp --dport 20333:20333 -j DROP
+docker exec -it eco-neo-csharp-node3-running iptables -D INPUT -p tcp --dport 20333:20333 -j ACCEPT
+docker exec -it eco-neo-csharp-node4-running iptables -D INPUT -p tcp --dport 20333:20333 -j ACCEPT
 
 echo "unblock specifics in node1"
-docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20335:20335 -j DROP
-docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20336:20336 -j DROP
+docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20335:20335 -j ACCEPT
+docker exec -it eco-neo-csharp-node1-running iptables -D OUTPUT -p tcp --dport 20336:20336 -j ACCEPT
