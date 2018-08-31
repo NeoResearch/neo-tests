f1865642011d449121e6b0f3cd9e8b6f5b43e988
Vitor <vncoelho@gmail.com>
2018-08-31 10:41:30 -0300

    preview2-10 with appLog

--- simple-rpc-node/neo-cli-dockerfile/docker_build.sh
+++ simple-rpc-node/neo-cli-dockerfile/docker_build.sh
@@ -4,7 +4,7 @@ set -e
 echo "Building ubuntu-dotnet dependent image"
 (cd ../../docker-ubuntu-dotnet; ./docker_build.sh)
 
-NEO_CLI_VERSION="3.0-preview9.1-appLog"
+NEO_CLI_VERSION="3.0-preview2-10-appLog"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"
