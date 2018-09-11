c3f491ddf550b281d4451d0b855bff5b7b4de36f
Vitor <vncoelho@gmail.com>
2018-09-11 20:07:28 -0300

    History and neocli 2.9.0

--- simple-rpc-node/neo-cli-dockerfile/scripts/start_node.sh
+++ simple-rpc-node/neo-cli-dockerfile/scripts/start_node.sh
@@ -4,6 +4,7 @@ cd /opt/node/neo-cli/
 if (($DISABLE_NETWORK_P2P)); then
 	echo "Disabling network P2P"
 	iptables -A INPUT -p tcp --dport 10333:60333 -j REJECT
+	iptables -A OUTPUT -p tcp --dport 10333:60333 -j REJECT
 fi
 
 if (($IS_RPC)); then
