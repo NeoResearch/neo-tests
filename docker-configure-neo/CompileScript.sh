65471692ef79ee154b0f4fc7644bc667db0c574e
Igor Machado <igor.machado@gmail.com>
2018-10-04 11:35:25 -0300

    added neo-vm compilation too

--- docker-configure-neo/CompileScript.sh
+++ docker-configure-neo/CompileScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "writing specifications"
-echo "build git project specifications" > /opt/build/projectspecs.log
+echo "build git project specifications" > /opt/build/projecspecs.log
 echo "neo-cli:" >> /opt/build/projecspecs.log
 (cd /opt/neo-cli && git show >> /opt/build/projecspecs.log)
 echo "neo:" >> /opt/build/projecspecs.log
@@ -17,12 +17,31 @@ mkdir -p /opt/neo-cli/neo-cli/neo-cli/Plugins/
 echo "mkdir /opt/neo-cli/neo-cli/neo-cli/Plugins-disabled/"
 rm -rf /opt/neo-cli/neo-cli/neo-cli/Plugins-disabled/
 mkdir -p /opt/neo-cli/neo-cli/neo-cli/Plugins-disabled/
+
+echo ""
+echo "================================================"
+echo "BUILDING plugin SimplePolicy"
+echo "================================================"
+echo ""
 PLUGIN_TO_INCLUDE_I="SimplePolicy"
 dotnet publish /opt/neo-plugins/$PLUGIN_TO_INCLUDE_I/$PLUGIN_TO_INCLUDE_I.csproj -o $PLUGIN_TO_INCLUDE_I -c Release -r ubuntu.16.04-x64 -f netstandard2.0
 cp -ri /opt/neo-plugins/$PLUGIN_TO_INCLUDE_I/$PLUGIN_TO_INCLUDE_I/$PLUGIN_TO_INCLUDE_I.dll /opt/neo-cli/neo-cli/neo-cli/Plugins/
 
+echo ""
+echo "================================================"
+echo "BUILDING plugin StatesDumper"
+echo "================================================"
+echo ""
+PLUGIN_TO_INCLUDE_II="StatesDumper"
+dotnet publish /opt/neo-plugins/$PLUGIN_TO_INCLUDE_II/$PLUGIN_TO_INCLUDE_II.csproj -o $PLUGIN_TO_INCLUDE_II -c Release -r ubuntu.16.04-x64 -f netstandard2.0
+cp -ri /opt/neo-plugins/$PLUGIN_TO_INCLUDE_II/$PLUGIN_TO_INCLUDE_II/$PLUGIN_TO_INCLUDE_II.dll /opt/neo-cli/neo-cli/neo-cli/Plugins/
+
 
 # remove neo-cli package and reference to local neo
+echo ""
+echo "================================================"
+echo "UPDATING REFERENCE OF PROJECT neo INSIDE neo-cli"
+echo "================================================"
 echo ""
 echo "dotnet remove /opt/neo-cli/neo-cli/neo-cli.csproj package neo"
 dotnet remove /opt/neo-cli/neo-cli/neo-cli.csproj package neo
@@ -35,6 +54,26 @@ echo ""
 echo "dotnet add /opt/neo-cli/neo-cli/neo-cli.csproj reference /opt/neo/neo/neo.csproj"
 dotnet add /opt/neo-cli/neo-cli/neo-cli.csproj reference /opt/neo/neo/neo.csproj
 
+# change neo-vm
+
+echo ""
+echo "==================================================="
+echo "UPDATING REFERENCE OF PROJECT neo-vm INSIDE neo"
+echo "==================================================="
+echo ""
+echo "dotnet remove /opt/neo/neo/neo.csproj package Neo.VM"
+dotnet remove /opt/neo/neo/neo.csproj package Neo.VM
+
+echo ""
+echo "dotnet sln /opt/neo/neo.sln add /opt/neo-vm/src/neo-vm/neo-vm.csproj"
+dotnet sln /opt/neo/neo.sln add /opt/neo-vm/src/neo-vm/neo-vm.csproj
+
+echo ""
+echo "dotnet add /opt/neo/neo/neo.csproj reference /opt/neo-vm/src/neo-vm/neo-vm.csproj"
+dotnet add /opt/neo/neo/neo.csproj reference /opt/neo-vm/src/neo-vm/neo-vm.csproj
+
+
+
 #(cd /opt/neo && dotnet clean)
 #(cd /opt/neo-cli && dotnet clean)
 
@@ -45,6 +84,11 @@ dotnet add /opt/neo-cli/neo-cli/neo-cli.csproj reference /opt/neo/neo/neo.csproj
 #msbuild /p:Configuration=Release
 #cp neo/bin/Release/netstandard2.0/* build/
 
+echo ""
+echo "==================================================="
+echo "BUILDING FINAL neo-cli"
+echo "==================================================="
+echo ""
 # publish
 echo ""
 echo "dotnet publish /opt/neo-cli/neo-cli/neo-cli.csproj -o neo-cli -c Release -r ubuntu.16.04-x64"
--- docker-configure-neo/docker_run.sh
+++ docker-configure-neo/docker_run.sh
@@ -11,15 +11,26 @@ IMAGE_NAME="neo-opt-compiler"
 
 NEO_BRANCH=Neo.Actor
 NEO_BRANCH_COMMIT=4280a5d7ac000bc94f5df5efde2f888aaac7a9c8
+NEO_URL=https://github.com/neo-project/neo.git
+
 NEO_CLI_BRANCH=v3.0
 NEO_CLI_BRANCH_COMMIT=b4b979fdeccdf85b2daf76ff98ddcda133d3f8cd
+NEO_CLI_URL=https://github.com/neo-project/neo-cli.git
+
 NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_BRANCH_COMMIT=095e89fb175e2b9a6c55bd0c88f7c039d2145d1c
-git clone -b $NEO_CLI_BRANCH https://github.com/neo-project/neo-cli.git neo-cli && (cd neo-cli && git checkout $NEO_CLI_BRANCH_COMMIT)
-git clone -b $NEO_BRANCH https://github.com/neo-project/neo.git neo-core && (cd neo-core && git checkout $NEO_BRANCH_COMMIT)
-git clone -b $NEO_PLUGINS_BRANCH https://github.com/neo-project/neo-plugins.git neo-plugins && (cd neo-plugins && git checkout $NEO_PLUGINS_BRANCH_COMMIT)
+NEO_PLUGINS_BRANCH_COMMIT=6f66abe005d98507e6ca06b705d820c5e8829e5c
+NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
+
+NEO_VM_BRANCH=patch-3
+NEO_VM_BRANCH_COMMIT=ca2d305ab33edf5262b098742097a787b7d3230a
+NEO_VM_URL=https://github.com/igormcoelho/neo-vm.git
+
+git clone -b $NEO_CLI_BRANCH $NEO_CLI_URL neo-cli && (cd neo-cli && git checkout $NEO_CLI_BRANCH_COMMIT)
+git clone -b $NEO_BRANCH $NEO_URL neo-core && (cd neo-core && git checkout $NEO_BRANCH_COMMIT)
+git clone -b $NEO_PLUGINS_BRANCH $NEO_PLUGINS_URL neo-plugins && (cd neo-plugins && git checkout $NEO_PLUGINS_BRANCH_COMMIT)
+git clone -b $NEO_VM_BRANCH $NEO_VM_URL neo-vm && (cd neo-vm && git checkout $NEO_VM_BRANCH_COMMIT)
 
 echo "Starting container..."
 #docker run --rm --volume neo-code:/neo-code --name $IMAGE_NAME -it /bin/bash
-echo "docker run --rm --volume=`pwd`/neo-core:/opt/neo --volume=`pwd`/neo-cli:/opt/neo-cli --volume=`pwd`/neo-plugins:/opt/neo-plugins --volume=`pwd`/build:/opt/build  neo-opt-compiler -t"
-docker run --rm --volume=`pwd`/neo-core:/opt/neo --volume=`pwd`/neo-cli:/opt/neo-cli --volume=`pwd`/neo-plugins:/opt/neo-plugins --volume=`pwd`/build:/opt/build  neo-opt-compiler -t
+echo "docker run --rm --volume=`pwd`/neo-core:/opt/neo --volume=`pwd`/neo-vm:/opt/neo-vm --volume=`pwd`/neo-cli:/opt/neo-cli --volume=`pwd`/neo-plugins:/opt/neo-plugins --volume=`pwd`/build:/opt/build  neo-opt-compiler -t"
+docker run --rm --volume=`pwd`/neo-core:/opt/neo --volume=`pwd`/neo-vm:/opt/neo-vm --volume=`pwd`/neo-cli:/opt/neo-cli --volume=`pwd`/neo-plugins:/opt/neo-plugins --volume=`pwd`/build:/opt/build  neo-opt-compiler -t
