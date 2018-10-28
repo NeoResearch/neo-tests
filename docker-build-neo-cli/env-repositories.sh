2e3be46ba1010c5eea735fad1aa3bb94d1e05206
Vitor <vncoelho@gmail.com>
2018-10-28 18:37:19 -0300

    Fixing automatic local files as parameter

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -10,9 +10,22 @@ NEO_BLOCKCHAIN_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
 NEO_PLUGINS_BRANCH=master
 NEO_PLUGINS_COMMIT=ada292dec3c93cd478a101937f99af84a36654b8
 
-LOCAL_NEO_BLOCKCHAIN="false"
-LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
+
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
+LOCAL_NEO_BLOCKCHAIN="true"
+LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo-igor"
 LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-plugins"
+
+if [ "$LOCAL_NEO_CLI_BLOCKCHAIN" != "true" ] ; then
+    LOCAL_NEO_CLI_BLOCKCHAIN_URL="./env-repositories.sh"
+fi
+
+if [ "$LOCAL_NEO_BLOCKCHAIN" != "true" ] ; then
+    LOCAL_NEO_BLOCKCHAIN_URL="./env-repositories.sh"
+fi
+
+if [ "$LOCAL_NEO_PLUGINS" != "true" ] ; then
+    LOCAL_NEO_PLUGINS_URL="./env-repositories.sh"
+fi
