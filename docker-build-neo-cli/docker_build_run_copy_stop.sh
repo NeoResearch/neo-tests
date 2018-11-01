95784a362e81e59379f29c583cb99b852ceeff7e
Vitor <vncoelho@gmail.com>
2018-11-01 15:22:01 -0300

    automatic get commit from github on process as well as other configurations

--- docker-build-neo-cli/docker_build_run_copy_stop.sh
+++ docker-build-neo-cli/docker_build_run_copy_stop.sh
@@ -2,7 +2,60 @@
 #
 # Build new neo-cli using new files of neo-blockchain
 #
-source ./env-repositories.sh
+GETONLINEGITHUB=0
+ENVFILE="env-repositories.sh"
+
+function usage {
+    echo "Usage: $0 [--github] [--envfile <env.sh>]"
+}
+
+while [[ "$#" > 0 ]]; do case $1 in
+    --github)
+	echo "PARAMETER GETONLINEGITHUB activated.";
+        GETONLINEGITHUB=1
+        shift
+        ;;
+    --envfile)
+	echo "GETTING ENV FILE DEFINED IN THE COMMAND";
+        ENVFILE=$2
+        if [[ -z $ENVFILE ]]; then
+            echo "Error: Please specify an env file"
+            usage
+            exit 1
+        fi
+        shift; shift
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+  esac;
+done
+
+if ((!$GETONLINEGITHUB)); then
+	source ./$ENVFILE
+else
+	source ./env-repositories-github.sh
+fi
+
+# ./env-repositories.sh is just an example file (a workaround) to by copied with the ADD command of the dockerfile in case no local files are going to be used
+if [ "$LOCAL_NEO_CLI_BLOCKCHAIN" != "true" ] ; then
+    LOCAL_NEO_CLI_BLOCKCHAIN_URL="./env-repositories.sh"
+fi
+
+if [ "$LOCAL_NEO_BLOCKCHAIN" != "true" ] ; then
+    LOCAL_NEO_BLOCKCHAIN_URL="./env-repositories.sh"
+fi
+
+if [ "$LOCAL_NEO_PLUGINS" != "true" ] ; then
+    LOCAL_NEO_PLUGINS_URL="./env-repositories.sh"
+fi
+
+if [ "$LOCAL_NEO_VM" != "true" ] ; then
+    LOCAL_NEO_VM_URL="./env-repositories.sh"
+fi
+
+ARGS="--build-arg NEO_VM_URL=$NEO_VM_URL --build-arg NEO_CLI_BLOCKCHAIN_URL=$NEO_CLI_BLOCKCHAIN_URL  --build-arg NEO_CLI_BRANCH=$NEO_CLI_BRANCH --build-arg NEO_CLI_COMMIT=$NEO_CLI_COMMIT --build-arg NEO_BLOCKCHAIN_URL=$NEO_BLOCKCHAIN_URL --build-arg NEO_BLOCKCHAIN_BRANCH=$NEO_BLOCKCHAIN_BRANCH --build-arg NEO_BLOCKCHAIN_COMMIT=$NEO_BLOCKCHAIN_COMMIT --build-arg NEO_PLUGINS_URL=$NEO_PLUGINS_URL --build-arg NEO_PLUGINS_BRANCH=$NEO_PLUGINS_BRANCH --build-arg NEO_PLUGINS_COMMIT=$NEO_PLUGINS_COMMIT --build-arg LOCAL_NEO_BLOCKCHAIN=$LOCAL_NEO_BLOCKCHAIN --build-arg LOCAL_NEO_BLOCKCHAIN_URL=$LOCAL_NEO_BLOCKCHAIN_URL --build-arg LOCAL_NEO_CLI_BLOCKCHAIN=$LOCAL_NEO_CLI_BLOCKCHAIN --build-arg LOCAL_NEO_CLI_BLOCKCHAIN_URL=$LOCAL_NEO_CLI_BLOCKCHAIN_URL --build-arg LOCAL_NEO_PLUGINS=$LOCAL_NEO_PLUGINS --build-arg LOCAL_NEO_PLUGINS_URL=$LOCAL_NEO_PLUGINS_URL --build-arg SET_NEO_VM=$SET_NEO_VM --build-arg NEO_VM_URL=$NEO_VM_URL --build-arg NEO_VM_BRANCH=$NEO_VM_BRANCH --build-arg NEO_VM_COMMIT=$NEO_VM_COMMIT --build-arg LOCAL_NEO_VM=$LOCAL_NEO_VM --build-arg LOCAL_NEO_VM_URL=$LOCAL_NEO_VM_URL"
 
 echo "Building ARGS are: $ARGS"
 
--- docker-build-neo-cli/env-repositories-commitphase.sh
+++ docker-build-neo-cli/env-repositories-commitphase.sh
@@ -1,18 +1,19 @@
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
 NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=ed6dcb2ed7b457bd151cf21c6e9b6f4912e62eca 
-NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=a64ca722afc40d40faede9b35c049233875b9071
+NEO_CLI_COMMIT=6a875d546a9917b984dc88e64828b887de1010f8 
+NEO_BLOCKCHAIN_URL=https://github.com/igormcoelho/neo.git
+NEO_BLOCKCHAIN_BRANCH=new_commit_phase
+NEO_BLOCKCHAIN_COMMIT=5dbc7605c19fa3b02d5f91e4e1186c6467707117
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
 NEO_PLUGINS_BRANCH=master
 NEO_PLUGINS_COMMIT=ada292dec3c93cd478a101937f99af84a36654b8
+
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
 LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo-igor"
-LOCAL_NEO_PLUGINS="true"
+LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-plugins"
 
 SET_NEO_VM="false"
