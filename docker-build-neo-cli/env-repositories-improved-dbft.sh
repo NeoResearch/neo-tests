44b73c187c7a362cfa306896694db85f5e7e141f
Vitor <vncoelho@gmail.com>
2019-01-16 13:01:39 -0200

    2.9.5 release

--- docker-build-neo-cli/env-repositories-improved-dbft.sh
+++ docker-build-neo-cli/env-repositories-improved-dbft.sh
@@ -1,9 +1,9 @@
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
 NEO_CLI_BRANCH=fix-OnShowPoolCommand
-NEO_CLI_COMMIT=da047c23175c7f5554e5aadb1b5cac4b6b151c36 
+NEO_CLI_COMMIT=3b0e7fbacb2b7890fd98bba4bf779652d4823084 
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=consensus/improved_dbft
-NEO_BLOCKCHAIN_COMMIT=c58cbfd50958f333aeb897f408ba23227fce9344
+NEO_BLOCKCHAIN_COMMIT=c90dec307b71d2efe056a19df5825a39907bb9fc
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
 NEO_PLUGINS_BRANCH=master
 NEO_PLUGINS_COMMIT=a7fd6590cea4530d2e2082aec0a1a6ea5bdd6451
