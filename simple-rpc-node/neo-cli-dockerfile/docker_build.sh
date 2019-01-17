a588dab6ff84751cc2af96edae4915df276a3a04
Vitor <vncoelho@gmail.com>
2019-01-17 11:41:21 -0200

    Update simple rpc release

--- simple-rpc-node/neo-cli-dockerfile/docker_build.sh
+++ simple-rpc-node/neo-cli-dockerfile/docker_build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-
-NEO_CLI_VERSION="2.9.4-all-plugins-commit-phase"
+NEO_CLI_VERSION="2.9.5-all-plugins-improved-dbft"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"
