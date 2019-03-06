89fab0e36fae0a8d421a08393ebcdbf5bf9953b8
Vitor <vncoelho@gmail.com>
2019-03-06 16:06:33 -0300

    neo-cli v2.10.0 test release

--- docker-build-neo-cli/env-repositories-dbft-2.0.sh
+++ docker-build-neo-cli/env-repositories-dbft-2.0.sh
@@ -1,12 +1,12 @@
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
-NEO_CLI_BRANCH=fix-OnShowPoolCommand
-NEO_CLI_COMMIT=1b845bb1a7da0635de24492e80a56a7e88f83cac 
+NEO_CLI_BRANCH=v2.10.0
+NEO_CLI_COMMIT=81d22c5f17f364372a0fe67c1c534f8af9e91b1a 
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=consensus/improved_dbft
-NEO_BLOCKCHAIN_COMMIT=445fa92a661184a1371aa6238ee7633be0d54f1e
+NEO_BLOCKCHAIN_BRANCH=master
+NEO_BLOCKCHAIN_COMMIT=8be5fa258f108f631451cb872a070a05e76b978c
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
-NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=e41d3441d43d9813bf6fa2a401bcbb9fb9b5267c
+NEO_PLUGINS_BRANCH=feature/RpcWallet
+NEO_PLUGINS_COMMIT=9360a45d48a7365daacac240b839533b189faf3c
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
@@ -24,8 +24,9 @@ LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
 PLUGINS_LIST="
 SimplePolicy
-ApplicationLogs
+RpcWallet
 StatesDumper
 ImportBlocks
+ApplicationLogs
 RpcSecurity
 "
