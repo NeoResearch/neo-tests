e7f90583a18e7c4552c14f371a40e6c0afcf2d98
Vitor <vncoelho@gmail.com>
2019-03-14 19:19:35 -0300

    Testing fine tuning

--- docker-build-neo-cli/env-repositories-fine-tuning.sh
+++ docker-build-neo-cli/env-repositories-fine-tuning.sh
@@ -1,16 +1,16 @@
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
-NEO_CLI_BRANCH=feature/maxGasInvoke
-NEO_CLI_COMMIT=5f92fe78c8ad01d87dd159e7bb7063b04efeb4f9 
+NEO_CLI_BRANCH=master
+NEO_CLI_COMMIT=8a12abdbbdc4b641ce938adfd958e7399d3d8cd4
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=bc14798fa20d68140c1605ae916a3587ec1288ac
+NEO_BLOCKCHAIN_BRANCH=fine-tunning-consensus-primary-timeouts
+NEO_BLOCKCHAIN_COMMIT=451338d43886701964029f8450325fa622146d45
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
-NEO_PLUGINS_BRANCH=RpcSystemAssetPlugin
-NEO_PLUGINS_COMMIT=dfa8f7da826a47aa43371f564380d0a1e87e90f4
+NEO_PLUGINS_BRANCH=plugins-2.10.1
+NEO_PLUGINS_COMMIT=e564e3dace203cb4578caac774a91413d7771229
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
-LOCAL_NEO_BLOCKCHAIN="false"
+LOCAL_NEO_BLOCKCHAIN="true"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
 LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-plugins"
@@ -22,8 +22,8 @@ NEO_VM_COMMIT=fa79898
 LOCAL_NEO_VM="false"
 LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
-# ApplicationLogs
 PLUGINS_LIST="
+ApplicationLogs
 SimplePolicy
 StatesDumper
 ImportBlocks
