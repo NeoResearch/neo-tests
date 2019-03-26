f6491eafc43b861f6116d8aac834be70dea9316a
Vitor <vncoelho@gmail.com>
2019-03-25 22:18:27 -0300

    Deleting old project that takes snapshots for getting storage

--- simple-storage-rpc/storage-rpc/run-StorageSnapshot-RPC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DOOR_STATE_SNAPSHOT=12000
-
-echo "fuser -k on port $DOOR_STATE_SNAPSHOT"
-fuser -k  $DOOR_STATE_SNAPSHOT/tcp
-
-echo "starting appStorageSnapshot.js at port $DOOR_STATE_SNAPSHOT"
-node appStateSnapshot.js
