876c4ab0228c755699dc70bbd1e88a2675e02f83
Vitor <vncoelho@gmail.com>
2019-04-11 15:25:55 -0300

    Preview for testing neo-vm

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,12 +1,12 @@
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
 NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=35ad64681cbf621bfa7ee8bbab197203f37e1553 
+NEO_CLI_COMMIT=ceaf911ca451e1771e8364b1235283d58942352c 
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=a69b0ce1ea217843e8f16dd37526a75b1fceac01
+NEO_BLOCKCHAIN_COMMIT=3a06f9c0efcc3cc267d380bb945fe7474b1f2243
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
 NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=b3d49a0990aa0d988f4c70af029c703c18c49a48
+NEO_PLUGINS_COMMIT=f4b83e05b38a9cacbecb298430a4016cd1468b4c
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
@@ -15,10 +15,10 @@ LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
 LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-plugins"
 
-SET_NEO_VM="false"
+SET_NEO_VM="true"
 NEO_VM_URL=https://github.com/neo-project/neo-vm.git
 NEO_VM_BRANCH=master
-NEO_VM_COMMIT=fa79898
+NEO_VM_COMMIT=f0f80906f90b507ec7f1fa4c8d992ebe2487b869
 LOCAL_NEO_VM="false"
 LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
