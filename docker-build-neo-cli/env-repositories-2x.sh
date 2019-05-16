21fbcb7167db555948f6d260597ab2fc1d5fe7a3
Vitor <vncoelho@gmail.com>
2019-05-16 11:46:11 -0300

    Neo release 2.10.2

--- docker-build-neo-cli/env-repositories-2x.sh
+++ docker-build-neo-cli/env-repositories-2x.sh
@@ -1,12 +1,12 @@
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
-NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=f6401766f69163c6dd3e1b76d152d86eace06983 
+NEO_CLI_BRANCH=master-2x
+NEO_CLI_COMMIT=55df26b1c41a2550b1f84cac44337a9dec885ed5 
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=a953456efefc3a8559fe9f65b1b06dd3da957f96
+NEO_BLOCKCHAIN_BRANCH=master-2.x
+NEO_BLOCKCHAIN_COMMIT=a52de9ba5c308d4c3e10271d4a1e45a313c6df32
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
-NEO_PLUGINS_BRANCH=minorfix-storage-state-dumper
-NEO_PLUGINS_COMMIT=fb584f5fb099fc31899d968e7e1eb66b5a5e5fee
+NEO_PLUGINS_BRANCH=master
+NEO_PLUGINS_COMMIT=14eb335917c592646c74b02c057e42d34646a615
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
@@ -15,12 +15,11 @@ LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
 LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-plugins"
 
-# port/scripthash
-# 762c911fc196aa9ce8ae2281905373784920e651
-SET_NEO_VM="true"
+SET_NEO_VM="false"
 NEO_VM_URL=https://github.com/neo-project/neo-vm.git
 NEO_VM_BRANCH=master-2.x
-NEO_VM_COMMIT=0d13635d37cab57288bc6d7e422f7e127459f99d
+NEO_VM_COMMIT=e9b0e6a6bd0cc709a00add2e128c81791fa6d352
+
 LOCAL_NEO_VM="false"
 LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
