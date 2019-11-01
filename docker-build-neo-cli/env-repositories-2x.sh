e04576b80fb3b39fff20b6d6de2674b7f06e3b85
Vitor <vncoelho@gmail.com>
2019-11-01 11:18:43 -0300

    New neo2x release and minor updates

--- docker-build-neo-cli/env-repositories-2x.sh
+++ docker-build-neo-cli/env-repositories-2x.sh
@@ -1,27 +1,27 @@
 # https://github.com/neo-project/neo-cli/commits/master-2.x
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
 NEO_CLI_BRANCH=master-2.x
-NEO_CLI_COMMIT=7420ccc294770d9aef47b4108f7439018c3f837f
+NEO_CLI_COMMIT=2119441d75e5ba2b138058a65b47557b78380287
 
 # https://github.com/neo-project/neo/commits/master-2.x
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master-2.x
-NEO_BLOCKCHAIN_COMMIT=be17769a13f9ebe921108d4694834b204575317c
+NEO_BLOCKCHAIN_COMMIT=f147946b9c3e7229f90e40e7dbc6a14c84d88fc5
 
 # https://github.com/neo-project/neo-plugins/commits/master-2.x
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
 NEO_PLUGINS_BRANCH=master-2.x
-NEO_PLUGINS_COMMIT=656283b17cc2b89c0b9bbcba67431c189e0a370f
+NEO_PLUGINS_COMMIT=b5388d753a2da1d59583dd9c66835e29ca7fd6f3
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
-LOCAL_NEO_BLOCKCHAIN="true"
+LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
 LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-plugins"
 
 # https://github.com/neo-project/neo-vm/commits/master-2.x
-SET_NEO_VM="true"
+SET_NEO_VM="false"
 NEO_VM_URL=https://github.com/neo-project/neo-vm.git
 NEO_VM_BRANCH=master-2.x
 NEO_VM_COMMIT=15170c46609ea61f6fcf6f36a90033bf658f2ab7
@@ -33,10 +33,10 @@ PLUGINS_LIST="
 ApplicationLogs
 CoreMetrics
 ImportBlocks
+RpcNep5Tracker
+RpcSecurity
+RpcSystemAssetTracker
+RpcWallet
 SimplePolicy
 StatesDumper
-RpcSecurity
-RpcWallet
-RpcNep5Tracker
-RpcSystemAssetTracker
 "
--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,21 +1,27 @@
+# https://github.com/neo-project/neo-cli/commits/master
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
-NEO_CLI_BRANCH=3.0/neo-122
-NEO_CLI_COMMIT=d19206bc743c71fdc81644bf7f88143fab744fa3
+NEO_CLI_BRANCH=master
+NEO_CLI_COMMIT=d20b5924473c3abba514803b1a3cf4f4ed0cb5d0
+
+# https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=a9f46bc7d351933e7d8666665c49f850ed5d8e24
+NEO_BLOCKCHAIN_COMMIT=0aed29f628ef2e49e9f728970c7970836243bb1a
+
+# https://github.com/neo-project/neo-plugins/commits/master
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
 NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=3774004b6a4bb45034458fd90698079640f6079e
+NEO_PLUGINS_COMMIT=90c15dc4de570e8de19769e5e95b04a86814ef59
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
 LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
-LOCAL_NEO_PLUGINS="true"
+LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-plugins"
 
-SET_NEO_VM="true"
+# https://github.com/neo-project/neo-vm/commits/master
+SET_NEO_VM="false"
 NEO_VM_URL=https://github.com/neo-project/neo-vm.git
 NEO_VM_BRANCH=master
 NEO_VM_COMMIT=332db08ada15916e69451fc9a23075574ac511a1
@@ -30,6 +36,6 @@ ImportBlocks
 RpcNep5Tracker
 RpcSecurity
 RpcWallet
-SimplePolicy
 StatesDumper
+SystemLog
 "
