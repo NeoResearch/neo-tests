027ab86dd1d1b10e7f33f952887a70193cb8e626
Vitor <vncoelho@gmail.com>
2020-01-09 07:50:35 -0300

    Updating commits to up-to-date - Built with sucess neo-cli

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-node/commits/master
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
-NEO_CLI_BRANCH=update-neo
-NEO_CLI_COMMIT=4bcf25da7a0d3fb9e3c81b129501465d0b404730
+NEO_CLI_BRANCH=master
+NEO_CLI_COMMIT=51cd29fbe21abb9e1f17f64e5c6d21bc7decbbb9
 
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=c5a0f07e14465d8be3afbfc506db4b064899edd4
+NEO_BLOCKCHAIN_COMMIT=040f3b8f2614831f7e3b1eaf0b0f2a072c23a06c
 
 # https://github.com/neo-project/neo-modules/commits/master
-NEO_PLUGINS_URL=https://github.com/chenquanyu/neo-modules.git
-NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=430b6d8cbf68a8a63cc0664694673a65b5ff0de7
+NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
+NEO_PLUGINS_BRANCH=update-rpc-client
+NEO_PLUGINS_COMMIT=d4342237b900736ad67bbfcf5feaa67b545aabb6
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
@@ -29,7 +29,6 @@ NEO_VM_COMMIT=c08e853d1314c596575043c47b2b6a72b70ce305
 LOCAL_NEO_VM="false"
 LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
-PLUGINS_LIST_NEO_PACKAGE=(LevelDBStore RpcClient RpcServer StatesDumper SystemLog)
-# RocksDBStore
+PLUGINS_LIST_NEO_PACKAGE=(LevelDBStore RocksDBStore RpcClient RpcServer StatesDumper SystemLog)
 PLUGINS_LIST_NO_NEO_PACKAGE=(ApplicationLogs RpcNep5Tracker)
 MODULES_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.Storage.Tests)
