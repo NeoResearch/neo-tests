e57c965d60f7ca0ee486af7f5ffe0fa1759e1cd3
Vitor <vncoelho@gmail.com>
2020-06-10 09:45:49 -0300

    New preview for master2x

--- docker-build-neo-cli/env-repositories-2x.sh
+++ docker-build-neo-cli/env-repositories-2x.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-cli/commits/master-2.x
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
 NEO_CLI_BRANCH=master-2.x
-NEO_CLI_COMMIT=a46e62e1c1e510a392ae67718f79729084df272b
+NEO_CLI_COMMIT=f81e60bac50c877f347bf04119b24d5afff158b7
 
 # https://github.com/neo-project/neo/commits/master-2.x
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=a1ea920accdecc9298ddaaff306071b3a4ae3d55
+NEO_BLOCKCHAIN_COMMIT=75df7cdaeffc84cd116d907ceda11054be696af3
 
-# https://github.com/neo-project/neo-plugins/commits/master-2.x
-NEO_PLUGINS_URL=https://github.com/neo-project/neo-plugins.git
-NEO_PLUGINS_BRANCH=master-2.x
-NEO_PLUGINS_COMMIT=ccc5d041f990161f0b95707133afab72548afb08
+# https://github.com/neo-project/neo-modules/commits/master-2.x
+NEO_PLUGINS_URL=https://github.com/joeqian10/neo-modules.git
+NEO_PLUGINS_BRANCH=close-251
+NEO_PLUGINS_COMMIT=e80fbafe7c0bd6d9e1d32533dc7a319e9bea634a
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-node/commits/master
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
 NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=4bfce3d9e3d6afaed34aefef9396f148682dbb70
+NEO_CLI_COMMIT=68f08e114bc87df1bbf8c7a9e4a8403d3e81f03f
 
-# https://github.com/neo-project/neo/commits/master
+# https://github.com/neo-project/neo-node/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=e14053695ece799d88a5228a5507840d4f76e4ec
+NEO_BLOCKCHAIN_COMMIT=44ccf0b2211aa3ee66e24265bf5353f0d4b2cdc8
 
 # https://github.com/neo-project/neo-modules/commits/master
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
 NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=3dcd2bdac59611ad5c71fcb4891f631919892d0a
+NEO_PLUGINS_COMMIT=c51b96bb8802f48a14e2677a027bed408a0a5b26
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
@@ -29,7 +29,7 @@ NEO_VM_COMMIT=754bca3568c50695c571cb796c500fd66c07d0fe
 LOCAL_NEO_VM="false"
 LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
-PLUGINS_LIST_NEO_PACKAGE=(RpcServer RpcClient LevelDBStore RocksDBStore StatesDumper SystemLog)
+PLUGINS_LIST_NEO_PACKAGE=(LevelDBStore RocksDBStore RpcClient RpcServer StatesDumper)
 PLUGINS_LIST_NO_NEO_PACKAGE=(ApplicationLogs RpcNep5Tracker)
 PLUGINS_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.Storage.Tests)
 
