55baa794b6bfef93337e51824591e61372d3f73f
Vitor <vncoelho@gmail.com>
2020-09-17 11:28:32 -0300

    Commits for 2.12.0 DeFi

--- docker-build-neo-cli/env-repositories-2x.sh
+++ docker-build-neo-cli/env-repositories-2x.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-cli/commits/master-2.x
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-cli.git
 NEO_CLI_BRANCH=master-2.x
-NEO_CLI_COMMIT=42d3daf2232ab3c00cce1247df01b09de0b6f51f
+NEO_CLI_COMMIT=6baec93adfecbae629cbceb627933d9273759ff0
 
 # https://github.com/neo-project/neo/commits/master-2.x
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=95596f93a604f48d0a962b092b143cbdf0769bf5
+NEO_BLOCKCHAIN_BRANCH=master-2.x
+NEO_BLOCKCHAIN_COMMIT=d29e310b6d0882bb3dc276d57da1f10121f71b02
 
 # https://github.com/neo-project/neo-modules/commits/master-2.x
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
-NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=28d33b3d3aea43b957c1cf3a3cd211779be13396
+NEO_PLUGINS_BRANCH=master-2.x
+NEO_PLUGINS_COMMIT=691aec626bef606bf50339f8b90819dfdce773d5
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-cli"
---
+++ docker-build-neo-cli/run_build_2x.sh
@@ -0,0 +1,1 @@
+./docker_build_run_copy_stop.sh --envfile ./env-repositories-2x.sh --neo2x
