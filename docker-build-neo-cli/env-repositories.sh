3a436a6672cd9a4e8d57d0c36697be3442df07c6
Vitor <vncoelho@gmail.com>
2021-01-26 17:57:00 -0300

    Adding new release of preview4

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-node/commits/master
-NEO_CLI_BLOCKCHAIN_URL=https://github.com/bettybao1209/neo-node.git
-NEO_CLI_BRANCH=add-totalsupply
-NEO_CLI_COMMIT=02aa8f0
+NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
+NEO_CLI_BRANCH=master
+NEO_CLI_COMMIT=9ab7f601449d74b3632407641da4e2712777cc75
 
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=df79b42bce1901b2ab1372ade24634e693f6252e 
+NEO_BLOCKCHAIN_COMMIT=677bd9648d40b31076ba78c88c8a48bc0e919f66 
 
 # https://github.com/neo-project/neo-modules/commits/master
-NEO_PLUGINS_URL=https://github.com/chenzhitong/neo-modules.git
-NEO_PLUGINS_BRANCH=getnativecontract
-NEO_PLUGINS_COMMIT=1147f72
+NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
+NEO_PLUGINS_BRANCH=master
+NEO_PLUGINS_COMMIT=34b068e9c489a80fddc6b363cec6adb2896d98fe
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-node"
@@ -31,8 +31,8 @@ LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
 # https://github.com/neo-project/neo-modules/tree/master/src
 # https://github.com/neo-project/neo-modules/tree/master/tests
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs LevelDBStore RocksDBStore RpcClient RpcNep17Tracker RpcServer StateService StatesDumper dBFT)
-PLUGINS_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs LevelDBStore OracleService RocksDBStore RpcNep17Tracker RpcClient RpcServer StateService StatesDumper dBFT)
+PLUGINS_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
 
 NEO_BLOCKCHAIN_RUN_TESTS="true"
 PLUGINS_RUN_TESTS="true"
