1a4203f7a39b82267d3a3de78fc9359aea254231
Vitor <vncoelho@gmail.com>
2021-01-31 10:46:03 -0300

    Adding new release ledgercontract moved everything

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-node/commits/master
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
 NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=9ab7f601449d74b3632407641da4e2712777cc75
+NEO_CLI_COMMIT=a7f06cab3f5cb3099ba7e75ba3d1e18d6eda337d
 
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=677bd9648d40b31076ba78c88c8a48bc0e919f66 
+NEO_BLOCKCHAIN_COMMIT=b9dfe526f00df2f5f4a617c53481ed3bd0051d85 
 
 # https://github.com/neo-project/neo-modules/commits/master
-NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
-NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=34b068e9c489a80fddc6b363cec6adb2896d98fe
+NEO_PLUGINS_URL=https://github.com/bettybao1209/neo-modules.git
+NEO_PLUGINS_BRANCH=sync_neo
+NEO_PLUGINS_COMMIT=5a3b9a1
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-node"
@@ -31,7 +31,7 @@ LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
 # https://github.com/neo-project/neo-modules/tree/master/src
 # https://github.com/neo-project/neo-modules/tree/master/tests
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs LevelDBStore OracleService RocksDBStore RpcNep17Tracker RpcClient RpcServer StateService StatesDumper dBFT)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs LevelDBStore OracleService RocksDBStore RpcNep17Tracker RpcClient RpcServer StateService StatesDumper DBFTPlugin)
 PLUGINS_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
 
 NEO_BLOCKCHAIN_RUN_TESTS="true"
