f5b348fec73c7b644370f5bc648975f618a87551
Vitor <vncoelho@gmail.com>
2021-02-03 10:19:35 -0300

    First preview 5 release with all plugins

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-node/commits/master
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
 NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=a7f06cab3f5cb3099ba7e75ba3d1e18d6eda337d
+NEO_CLI_COMMIT=6389ab70592c5188a1bb88a5ce8aa8084a144cc5
 
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=b9dfe526f00df2f5f4a617c53481ed3bd0051d85 
+NEO_BLOCKCHAIN_COMMIT=604541c91b3151ee40a8613cb8534ebc0540fd27 
 
 # https://github.com/neo-project/neo-modules/commits/master
-NEO_PLUGINS_URL=https://github.com/bettybao1209/neo-modules.git
-NEO_PLUGINS_BRANCH=sync_neo
-NEO_PLUGINS_COMMIT=5a3b9a1
+NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
+NEO_PLUGINS_BRANCH=master
+NEO_PLUGINS_COMMIT=cc8c218163dd3c86c23dfa7e8b6adc411c4d545c
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-node"
@@ -31,7 +31,7 @@ LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
 # https://github.com/neo-project/neo-modules/tree/master/src
 # https://github.com/neo-project/neo-modules/tree/master/tests
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs LevelDBStore OracleService RocksDBStore RpcNep17Tracker RpcClient RpcServer StateService StatesDumper DBFTPlugin)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore OracleService RocksDBStore RpcClient RpcNep17Tracker RpcServer StateService StatesDumper)
 PLUGINS_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
 
 NEO_BLOCKCHAIN_RUN_TESTS="true"
