d953452921f652ae67aa0744e32c2432153b42e7
Vitor <vncoelho@gmail.com>
2021-10-28 12:35:54 -0300

    V4 version of 3.0.3

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -4,14 +4,14 @@ NEO_CLI_BRANCH=master
 NEO_CLI_COMMIT=8153553dcd8d68c752b9988ce7d7b9e360529b65
 
 # https://github.com/neo-project/neo/commits/master
-NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=a5dfdeb1f814494a1788713b1c86260e57751ced 
+NEO_BLOCKCHAIN_URL=https://github.com/Qiao-Jin/neo.git
+NEO_BLOCKCHAIN_BRANCH=taskmanager_optimization
+NEO_BLOCKCHAIN_COMMIT=456fe0242001eec2ce92b44fbba84c5d5e9393b1 
 
 # https://github.com/neo-project/neo-modules/commits/master
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
 NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=575efd238bf0bac87e4cef390b41c95ba6c07f3f
+NEO_PLUGINS_COMMIT=d0f143ac43fe6d4496430c01f737f3dd9f232904
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-node"
@@ -31,6 +31,7 @@ LOCAL_NEO_VM_URL="localfiles/neo-vm"
 
 # https://github.com/neo-project/neo-modules/tree/master/src
 # https://github.com/neo-project/neo-modules/tree/master/tests
+# RpcNep11Tracker
 PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore OracleService RocksDBStore RpcClient RpcNep17Tracker RpcServer StateService StatesDumper)
 PLUGINS_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
 
