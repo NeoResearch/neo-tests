ef48d51a7f9194a99885ec819a73c90ac2c2783e
Vitor <vncoelho@gmail.com>
2021-11-20 10:53:41 -0300

    Neo 3.1.0 with Dotnet 6.0 preview1

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-node/commits/master
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
 NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=8153553dcd8d68c752b9988ce7d7b9e360529b65
+NEO_CLI_COMMIT=24bd2ccfb7abd6ccdcb2e539a9896d1c3b02a4a7
 
 # https://github.com/neo-project/neo/commits/master
-NEO_BLOCKCHAIN_URL=https://github.com/Qiao-Jin/neo.git
-NEO_BLOCKCHAIN_BRANCH=taskmanager_optimization
-NEO_BLOCKCHAIN_COMMIT=456fe0242001eec2ce92b44fbba84c5d5e9393b1 
+NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
+NEO_BLOCKCHAIN_BRANCH=master
+NEO_BLOCKCHAIN_COMMIT=73b2192ebb67f31928c782ddf0777ad75ec6a8f6 
 
 # https://github.com/neo-project/neo-modules/commits/master
-NEO_PLUGINS_URL=https://github.com/Ashuaidehao/neo-modules.git
-NEO_PLUGINS_BRANCH=add-nep11
-NEO_PLUGINS_COMMIT=7556a43
+NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
+NEO_PLUGINS_BRANCH=net6
+NEO_PLUGINS_COMMIT=e283bb7
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-node"
@@ -32,8 +32,8 @@ LOCAL_NEO_VM_URL="localfiles/neo-vm"
 # https://github.com/neo-project/neo-modules/tree/master/src
 # https://github.com/neo-project/neo-modules/tree/master/tests
 # RpcNep11Tracker
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore OracleService RocksDBStore RpcClient RpcNep11Tracker RpcNep17Tracker RpcServer StateService StatesDumper)
-PLUGINS_UT_LIST=(Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore MPTTrie OracleService RocksDBStore RpcClient RpcNep17Tracker RpcServer StateService StatesDumper)
+PLUGINS_UT_LIST=(Neo.Cryptography.MPTTrie.Tests Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
 
-NEO_BLOCKCHAIN_RUN_TESTS="false"
-PLUGINS_RUN_TESTS="false"
+NEO_BLOCKCHAIN_RUN_TESTS="true"
+PLUGINS_RUN_TESTS="true"
