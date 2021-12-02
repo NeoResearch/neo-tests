0da8b2e3ab067a12d2341dc1d3e3d39e391a0d5a
Vitor <vncoelho@gmail.com>
2021-12-02 08:06:36 -0300

    3.1.0 release

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,17 +1,17 @@
 # https://github.com/neo-project/neo-node/commits/master
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
 NEO_CLI_BRANCH=master
-NEO_CLI_COMMIT=24bd2ccfb7abd6ccdcb2e539a9896d1c3b02a4a7
+NEO_CLI_COMMIT=b56a2465b353a2ad36091f5bda2a5752b7c2b933
 
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=73b2192ebb67f31928c782ddf0777ad75ec6a8f6 
+NEO_BLOCKCHAIN_COMMIT=2b7473799958d084e836d02ed9adbfd0fbcde8ce 
 
 # https://github.com/neo-project/neo-modules/commits/master
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
-NEO_PLUGINS_BRANCH=net6
-NEO_PLUGINS_COMMIT=e283bb7
+NEO_PLUGINS_BRANCH=master
+NEO_PLUGINS_COMMIT=b74d1d49dccabf738820f5ffcee4a844c19514e7
 
 LOCAL_NEO_CLI_BLOCKCHAIN="false"
 LOCAL_NEO_CLI_BLOCKCHAIN_URL="localfiles/neo-node"
@@ -32,8 +32,8 @@ LOCAL_NEO_VM_URL="localfiles/neo-vm"
 # https://github.com/neo-project/neo-modules/tree/master/src
 # https://github.com/neo-project/neo-modules/tree/master/tests
 # RpcNep11Tracker
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore MPTTrie OracleService RocksDBStore RpcClient RpcNep17Tracker RpcServer StateService StatesDumper)
-PLUGINS_UT_LIST=(Neo.Cryptography.MPTTrie.Tests Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.StateService.Tests Neo.Plugins.Storage.Tests)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore MPTTrie OracleService RocksDBStore RpcClient RpcServer StateService StatesDumper TokensTracker)
+PLUGINS_UT_LIST=(Neo.Cryptography.MPTTrie.Tests Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests  Neo.Plugins.RpcServer.Tests  Neo.Plugins.Storage.Tests)
 
 NEO_BLOCKCHAIN_RUN_TESTS="true"
 PLUGINS_RUN_TESTS="true"
