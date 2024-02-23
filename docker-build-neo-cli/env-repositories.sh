a7305bf20a0849a81bf3ef4eb1420ec2853406f5
Vitor Nazario Coelho <vncoelho@gmail.com>
2024-02-23 10:46:59 -0300

    Several fixes in order to ajust to current coverall and submodule as dep

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,19 +1,19 @@
 # https://github.com/neo-project/neo/commits/master
-NEO_BLOCKCHAIN_URL=https://github.com/cschuchardt88/neo.git
-NEO_BLOCKCHAIN_BRANCH=fix-neo-cli
-NEO_BLOCKCHAIN_COMMIT=010eb73 
+NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
+NEO_BLOCKCHAIN_BRANCH=master
+NEO_BLOCKCHAIN_COMMIT=b467613884ef4196283f9f303897dedb0c3de36d
 
 # https://github.com/neo-project/neo-modules/commits/master
-NEO_PLUGINS_URL=https://github.com/cschuchardt88/neo-modules.git
-NEO_PLUGINS_BRANCH=RestServer
-NEO_PLUGINS_COMMIT=68c4dfe3625583a21725457184c8c5f9ace65722
+NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
+NEO_PLUGINS_BRANCH=master
+NEO_PLUGINS_COMMIT=d69f1c9d682f067610a3cd74330ee3c4316f9435
 
 LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
 LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-modules"
 
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs LevelDBStore MPTTrie OracleService RocksDBStore RpcClient RestServer RpcServer SQLiteWallet StateService StatesDumper TokensTracker DBFTPlugin)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs LevelDBStore MPTTrie OracleService RocksDBStore RpcClient RpcServer SQLiteWallet StateService StatesDumper TokensTracker DBFTPlugin)
 PLUGINS_UT_LIST=(Neo.Cryptography.MPTTrie.Tests Neo.Network.RPC.Tests Neo.Plugins.OracleService.Tests Neo.Plugins.RpcServer.Tests  Neo.Plugins.Storage.Tests)
   
 NEO_BLOCKCHAIN_RUN_TESTS="true"
