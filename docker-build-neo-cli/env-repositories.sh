8bd4458ef45674a5e5127e2c362f2c2793a3f326
Vitor Nazario Coelho <vncoelho@gmail.com>
2024-05-14 08:48:30 -0300

    3.7.1

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,16 +1,16 @@
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=c550fc2
+NEO_BLOCKCHAIN_COMMIT=1dec0c73dc73127d9703b2f0c1c7a62d614db403
 
 # https://github.com/neo-project/neo-modules/commits/master
-NEO_PLUGINS_URL=https://github.com/cschuchardt88/neo-modules.git
-NEO_PLUGINS_BRANCH=dotnet-upgrade
-NEO_PLUGINS_COMMIT=da29890
+NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
+NEO_PLUGINS_BRANCH=master
+NEO_PLUGINS_COMMIT=dd6f9c4d1f0244ceac2dd1197dfc525fd9ad0011
 
-LOCAL_NEO_BLOCKCHAIN="true"
+LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
-LOCAL_NEO_PLUGINS="true"
+LOCAL_NEO_PLUGINS="false"
 LOCAL_NEO_PLUGINS_URL="localfiles/neo-modules-cris"
 
 PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore MPTTrie OracleService RocksDBStore RpcClient RpcServer SQLiteWallet StateService StatesDumper TokensTracker )
