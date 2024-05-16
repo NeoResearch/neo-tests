b0ae90c9d834af9982f44611ce15b090c3989962
Vitor Nazario Coelho <vncoelho@gmail.com>
2024-05-16 19:56:33 -0300

    3.7.4

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,12 +1,12 @@
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
 NEO_BLOCKCHAIN_BRANCH=master
-NEO_BLOCKCHAIN_COMMIT=1dec0c73dc73127d9703b2f0c1c7a62d614db403
+NEO_BLOCKCHAIN_COMMIT=7f227a3026fadeb9723280e6f961b6f29b0f8a7a
 
 # https://github.com/neo-project/neo-modules/commits/master
 NEO_PLUGINS_URL=https://github.com/neo-project/neo-modules.git
 NEO_PLUGINS_BRANCH=master
-NEO_PLUGINS_COMMIT=dd6f9c4d1f0244ceac2dd1197dfc525fd9ad0011
+NEO_PLUGINS_COMMIT=1577ad43941bd4bbe61d31e1843dca697d1f2da0
 
 LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
