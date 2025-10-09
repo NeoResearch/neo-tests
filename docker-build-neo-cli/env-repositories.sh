d09376de6dba18dc5597c72f1f746584abe3c513
Vitor <vncoelho@gmail.com>
2025-10-09 11:07:33 -0300

    dev up to date

--- docker-build-neo-cli/env-repositories.sh
+++ docker-build-neo-cli/env-repositories.sh
@@ -1,12 +1,12 @@
 # https://github.com/neo-project/neo/commits/master
 NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=divideStateService
-NEO_BLOCKCHAIN_COMMIT=bf1d3586021ba552c50e5a3a95cd7e694da7f626
+NEO_BLOCKCHAIN_BRANCH=dev
+NEO_BLOCKCHAIN_COMMIT=5321dfe4dfc75479346abbe3e6dd70306d43df1b
 
 LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_URL="localfiles/neo"
 
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore OracleService RestServer RocksDBStore RpcServer SQLiteWallet SignClient StateRootPlugin StateRpcPlugin StorageDumper TokensTracker)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore OracleService RestServer RocksDBStore RpcServer SQLiteWallet SignClient StateService StorageDumper TokensTracker)
   
 NEO_BLOCKCHAIN_RUN_TESTS="true"
 PLUGINS_RUN_TESTS="true"
