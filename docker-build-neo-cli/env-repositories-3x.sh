85b152e7a99d6cb64e17cb2aee7753bc5906cf26
Vitor <vncoelho@gmail.com>
2026-01-20 20:59:50 -0300

    3.9.1

--- docker-build-neo-cli/env-repositories-3x.sh
+++ docker-build-neo-cli/env-repositories-3x.sh
@@ -1,12 +1,12 @@
 # https://github.com/neo-project/neo-node/commits/master-n3
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
 NEO_CLI_BRANCH=master-n3
-NEO_CLI_COMMIT=419c3f1d8ab8ab18bd702518b5590e6f9acfd2c4
+NEO_CLI_COMMIT=60b988597a3421e2741bc02167ef7d7741f49cc0
 
 # https://github.com/neo-project/neo/commits/master-n3
-NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
-NEO_BLOCKCHAIN_BRANCH=master-n3
-NEO_BLOCKCHAIN_COMMIT=004c97efd6629bef2feb48d72678d73cde114644
+NEO_BLOCKCHAIN_URL=https://github.com/AnnaShaleva/neo.git
+NEO_BLOCKCHAIN_BRANCH=fix-recover-fund
+NEO_BLOCKCHAIN_COMMIT=48120419c3074304a9c925997039defac0d49a5e
 
 LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_PATH="localfiles/neo"
@@ -17,10 +17,10 @@ LOCAL_NEO_CLI_BLOCKCHAIN_PATH="localfiles/neo-node"
 # https://github.com/neo-project/neo-vm/commits/master
 NEO_VM_URL=https://github.com/neo-project/neo-vm.git
 NEO_VM_BRANCH=master
-NEO_VM_COMMIT=49f87f8ba5403719133c1bc59cba5a1d85ae729a
+NEO_VM_COMMIT=027baadb73dd894755250f63158bcbbdd8fcac33
 
 # If Set to  True it will download from github, otherwise it will not
-SET_NEO_VM="false"
+SET_NEO_VM="true"
 # INJECT LOCAL NEO-VM
 LOCAL_NEO_VM="false"
 LOCAL_NEO_VM_PATH="localfiles/neo-vm"
