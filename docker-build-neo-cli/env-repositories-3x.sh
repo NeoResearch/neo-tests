8fa886049813c1a092f1a19d427beb72ca067434
vncoelho <vncoelho@gmail.com>
2026-06-15 16:53:24 -0300

    3.10

--- docker-build-neo-cli/env-repositories-3x.sh
+++ docker-build-neo-cli/env-repositories-3x.sh
@@ -1,12 +1,12 @@
 # https://github.com/neo-project/neo-node/commits/master-n3
 NEO_CLI_BLOCKCHAIN_URL=https://github.com/neo-project/neo-node.git
 NEO_CLI_BRANCH=master-n3
-NEO_CLI_COMMIT=60b988597a3421e2741bc02167ef7d7741f49cc0
+NEO_CLI_COMMIT=203f37b0c2fd4856208aac60408edd45a1de9e88
 
 # https://github.com/neo-project/neo/commits/master-n3
-NEO_BLOCKCHAIN_URL=https://github.com/AnnaShaleva/neo.git
-NEO_BLOCKCHAIN_BRANCH=fix-recover-fund
-NEO_BLOCKCHAIN_COMMIT=48120419c3074304a9c925997039defac0d49a5e
+NEO_BLOCKCHAIN_URL=https://github.com/neo-project/neo.git
+NEO_BLOCKCHAIN_BRANCH=master-n3
+NEO_BLOCKCHAIN_COMMIT=df4026753787c93adf978a94dc63685cf54d2c82
 
 LOCAL_NEO_BLOCKCHAIN="false"
 LOCAL_NEO_BLOCKCHAIN_PATH="localfiles/neo"
@@ -20,14 +20,14 @@ NEO_VM_BRANCH=master
 NEO_VM_COMMIT=027baadb73dd894755250f63158bcbbdd8fcac33
 
 # If Set to  True it will download from github, otherwise it will not
-SET_NEO_VM="true"
+SET_NEO_VM="false"
 # INJECT LOCAL NEO-VM
 LOCAL_NEO_VM="false"
 LOCAL_NEO_VM_PATH="localfiles/neo-vm"
 # ========= INJECT SPECIFIC NEO-VM =========
 
 
-PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin LevelDBStore MPTTrie OracleService RestServer RocksDBStore RpcClient RpcServer SQLiteWallet SignClient StateService StorageDumper TokensTracker)
+PLUGINS_LIST_NEO_PACKAGE=(ApplicationLogs DBFTPlugin DeferredRelay LevelDBStore MPTTrie OracleService RestServer RocksDBStore RpcClient RpcServer SQLiteWallet SignClient StateService StorageDumper TokensTracker)
   
 NEO_BLOCKCHAIN_RUN_TESTS="true"
 PLUGINS_RUN_TESTS="true"
